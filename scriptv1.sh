686400e2691dada361458384d32885dd8cfca8ff
EC2 Default User <ec2-user@ip-172-31-3-98.us-west-2.compute.internal>
2021-12-12 09:01:34 +0000

    Commit

--- scriptv1.sh
+++ scriptv1.sh
@@ -16,11 +16,11 @@ aws configure set default.region ${AWS_REGION}
 #install kubectl
 
 #sudo curl --silent --location -o /usr/local/bin/kubectl https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/kubectl
-sudo curl --silent --location -o /usr/local/bin/kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/kubectl
+sudo curl --silent --location -o /usr/local/bin/kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.18.9/2020-11-02/bin/linux/amd64/kubectl
 sudo chmod +x /usr/local/bin/kubectl
 
 #install aws-iam-authenticator
-#curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/aws-iam-authenticator
+
 
 sudo curl --silent --location -o /usr/local/bin/aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/aws-iam-authenticator
 sudo chmod +x /usr/local/bin/aws-iam-authenticator
@@ -60,6 +60,7 @@ kind: ClusterConfig
 metadata:
   name: ${AWS_CLUSTER_NAME}
   region: ${AWS_REGION}
+  version: 1.18
 
 managedNodeGroups:
 - name: nodegroup
