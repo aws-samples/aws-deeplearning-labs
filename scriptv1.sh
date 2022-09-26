bd9b691fd49c6d9c03d9622a0c2d0a847ab7b815
Kalawat <kwt@3c06302d8399.ant.amazon.com>
2022-09-26 18:39:25 -0400

    Commit

--- scriptv1.sh
+++ scriptv1.sh
@@ -6,11 +6,8 @@ source ~/.bashrc
 export CLUSTER_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
 CLUSTER_RAND=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 3 ; echo '')
 export CLUSTER_NAME=eks-$CLUSTER_RAND
-aws configure --profile=kubeflow set default.region ${CLUSTER_REGION}
-export AWS_PROFILE=kubeflow
 export KUBEFLOW_RELEASE_VERSION=v1.6.0
 export AWS_RELEASE_VERSION=main
-aws sts get-caller-identity
 
 git clone https://github.com/awslabs/kubeflow-manifests.git && cd kubeflow-manifests
 git checkout ${AWS_RELEASE_VERSION}
@@ -27,6 +24,8 @@ EOF
 echo "export AWS_CLUSTER_NAME=${CLUSTER_NAME}" | tee -a ~/.bash_profile
 echo "export AWS_REGION=${CLUSTER_REGION}" | tee -a ~/.bash_profile
 
+aws configure --profile=kubeflow set default.region ${CLUSTER_REGION}
+export AWS_PROFILE=kubeflow
 aws sts get-caller-identity
 
 terraform init && terraform plan
