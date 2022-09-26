9dd99ef5ecd746edfd9383c2ebd7676f8223f967
Kalawat <kwt@3c06302d8399.ant.amazon.com>
2022-09-26 19:54:28 -0400

    Commit

--- scriptv2.sh
+++ scriptv2.sh
@@ -8,15 +8,15 @@ source ~/.bashrc
 nvm install node
 
 #Install Node.JS for ec2-user
-/bin/su -c "curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash" - ec2-user
-/bin/su -c "source ~/.nvm/nvm.sh" - ec2-user
-/bin/su -c "source ~/.bashrc" - ec2-user
-/bin/su -c "nvm install node" - ec2-user
+/bin/su -c "curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash" - ubuntu
+/bin/su -c "source ~/.nvm/nvm.sh" - ubuntu
+/bin/su -c "source ~/.bashrc" - ubuntu
+/bin/su -c "nvm install node" - ubuntu
 
 #Install Dev Tools
 sudo apt update
 
 # Install Cloud9 with Ec2-user
 
-sudo /bin/su -c "curl -L https://raw.githubusercontent.com/c9/install/master/install.sh | bash" - ec2-user
-sudo /bin/su -c "wget -O - https://raw.githubusercontent.com/c9/install/master/install.sh | bash" - ec2-user
+sudo /bin/su -c "curl -L https://raw.githubusercontent.com/c9/install/master/install.sh | bash" - ubuntu
+sudo /bin/su -c "wget -O - https://raw.githubusercontent.com/c9/install/master/install.sh | bash" - ubuntu
