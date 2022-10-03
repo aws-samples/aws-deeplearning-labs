fd7336b049f758f0175d9957568b093b0406fab0
Kalawat <kwt@3c06302d8399.ant.amazon.com>
2022-10-03 17:52:55 -0400

    Commit

--- install_eks_kubeflow.sh
+++ install_eks_kubeflow.sh
@@ -43,6 +43,24 @@ aws iam attach-role-policy --role-name ${NODE_IAM_ROLE} --policy-arn arn:aws:iam
 aws iam attach-role-policy --role-name ${NODE_IAM_ROLE} --policy-arn arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess
 aws iam attach-role-policy --role-name ${NODE_IAM_ROLE} --policy-arn arn:aws:iam::aws:policy/IAMReadOnlyAccess
 
-#cd /home/ubuntu/environment/eks-kubeflow-cloudformation-quick-start
-#sh profile-pod-default.sh
+# Create SageMaker Execution role
 
+cat << EoF > trust.json
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "sagemaker.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
+EoF
+
+aws iam create-role --role-name sagemakerrole --assume-role-policy-document file://trust.json --output=text
+aws iam attach-role-policy --role-name sagemakerrole --policy-arn arn:aws:iam::aws:policy/AmazonSageMakerFullAccess
+aws iam attach-role-policy --role-name sagemakerrole --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess
